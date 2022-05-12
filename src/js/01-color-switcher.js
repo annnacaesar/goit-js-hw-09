076aeb6741565430a6e7cf7553fd211c47775451
annnacaesar <Anechka22334@gmail.com>
2022-05-13 00:03:57 +0300

    first task complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,27 @@
-const btnStart = document.querySelector('data-start');
-console.log(btnStart);
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+btnStart.addEventListener('click', onChangeColor);
+btnStop.addEventListener('click', onStopChangeColor);
+
+let colorId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeColor() {
+	if (setInterval) {
+		btnStart.setAttribute("disabled", "disabled");
+	}
+	colorId = setInterval(() => {
+		body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopChangeColor () {
+	clearInterval(colorId);
+	btnStart.removeAttribute("disabled", "disabled");
+}
+
